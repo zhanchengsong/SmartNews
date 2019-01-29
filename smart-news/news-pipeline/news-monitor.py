c76052a29a2acd076b72e63ea9bc607ecda0e97b
songzhancheng <zhanchengdev@gmail.com>
2019-01-29 14:23:06 -0500

    Implemented CNN model for news classification and training

--- smart-news/news-pipeline/news-monitor.py
+++ smart-news/news-pipeline/news-monitor.py
@@ -29,19 +29,23 @@ while True:
     news_list = news_api_client.getNewsFromSource()
     num_of_new = 0
     for news in news_list:
-        news_digest = hashlib.md5(news['title'].encode('utf-8')).digest().encode('base64')
-        if redis_client.get(news_digest) is None:
-            num_of_new = num_of_new + 1 
-            news['digest'] = news_digest
-            print(news_digest)
-            if news['publishedAt'] is None: 
-                news['publishedAt'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
-            print(news)
-            redis_client.set(news_digest, json.dumps(news))
-            redis_client.expire(news_digest, NEWS_TIMEOUT_IN_SECONDS)
-            print("[News-Monitor] SEND MSG : ")
-            print(news)
-            CloudAMPQ_client.sendMessage(news)
+        if news != None and news['title'] != None:
+            news_digest = hashlib.md5(news['title'].encode('utf-8')).digest().encode('base64')
+            if redis_client.get(news_digest) is None:
+                num_of_new = num_of_new + 1
+                news['digest'] = news_digest
+                print(news_digest)
+                if news['publishedAt'] is None:
+                    news['publishedAt'] = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+                print(news)
+                redis_client.set(news_digest, json.dumps(news))
+                redis_client.expire(news_digest, NEWS_TIMEOUT_IN_SECONDS)
+                print("[News-Monitor] SEND MSG : ")
+                print(news)
+                CloudAMPQ_client.sendMessage(news)
+        else:
+            pass
+
     print ("Fetched %d new news ." % num_of_new)
     CloudAMPQ_client.sleep(SLEEP_IN_SECONDS)
 
