978429ac5f7765cca8f14aeecedf9d64626a23d2
songzhancheng <zhanchengdev@gmail.com>
2019-02-01 14:58:30 -0500

    Added services startup scripts

--- smart-news/start_services.sh
+++ smart-news/start_services.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 python ./classifying_model_service/server/classifying_server.py &
 python ./recommendation_service/server/recommendation_service.py &
-python ./recommendation_service/
+python ./recommendation_service/server/click_log_processor.py &
+
+python ./backend_server/service.py &
